18ae6c55e2d3fa19bd4381a37417a326b3358601
artur_fjodorov <artur.fodorov@gmail.com>
2020-03-19 04:20:55 +0300

    introducing font

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="home">
     <SvgIcon name="logo" />
+    <h1 class="h0">
+      h1 Artur <br />Fedorov
+    </h1>
+    <h1>h1 Artur Fedorov</h1>
+    <h2>h2 Artur Fedorov</h2>
+    <h3>h3 Artur Fedorov</h3>
+    <p>18px Testing base text</p>
+    <p class="p-body">16px Testing base text</p>
+    <p class="caption">12px Testing base text</p>
   </div>
 </template>
 
