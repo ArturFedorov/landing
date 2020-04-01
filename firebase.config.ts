907f2daae6d3d3796ecf35f38ccd67744723a044
artur_fjodorov <artur.fodorov@gmail.com>
2020-04-01 18:24:35 +0300

    firebase fix

--- firebase.config.ts
+++ firebase.config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyAiwi9eK8vQ30qSR4Fw3ZbavMZTLcqmUiM",
-  authDomain: "elena-4f241.firebaseapp.com",
-  databaseURL: "https://elena-4f241.firebaseio.com",
-  projectId: "elena-4f241",
-  storageBucket: "elena-4f241.appspot.com",
-  messagingSenderId: "723293874923",
-  appId: "1:723293874923:web:5e015817e67dd53a4cacae"
+  apiKey: 'AIzaSyAiwi9eK8vQ30qSR4Fw3ZbavMZTLcqmUiM',
+  authDomain: 'elena-4f241.firebaseapp.com',
+  databaseURL: 'https://elena-4f241.firebaseio.com',
+  projectId: 'elena-4f241',
+  storageBucket: 'elena-4f241.appspot.com',
+  messagingSenderId: '723293874923',
+  appId: '1:723293874923:web:5e015817e67dd53a4cacae'
 };
