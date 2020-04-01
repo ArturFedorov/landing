8593edb65f8651e282ba027c7a996041bb6098de
artur_fjodorov <artur.fodorov@gmail.com>
2020-04-01 18:10:47 +0300

    firebase

--- firebase.config.ts
+++ firebase.config.ts
@@ -1,10 +1,9 @@
 export const firebaseConfig = {
-  apiKey: 'AIzaSyD2Jo3nNiiR4cTnZExemFKlacYcDzOu4D8',
-  authDomain: 'af-portfolio-e4d15.firebaseapp.com',
-  databaseURL: 'https://af-portfolio-e4d15.firebaseio.com',
-  projectId: 'af-portfolio-e4d15',
-  storageBucket: 'af-portfolio-e4d15.appspot.com',
-  messagingSenderId: '3664023033',
-  appId: '1:3664023033:web:a23d35be2fcd8efe57c128',
-  measurementId: 'G-F1SF9TPT2E'
+  apiKey: "AIzaSyAiwi9eK8vQ30qSR4Fw3ZbavMZTLcqmUiM",
+  authDomain: "elena-4f241.firebaseapp.com",
+  databaseURL: "https://elena-4f241.firebaseio.com",
+  projectId: "elena-4f241",
+  storageBucket: "elena-4f241.appspot.com",
+  messagingSenderId: "723293874923",
+  appId: "1:723293874923:web:5e015817e67dd53a4cacae"
 };
